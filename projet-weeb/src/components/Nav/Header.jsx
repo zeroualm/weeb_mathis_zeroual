e2aa9a9db3bc6c4419ceda453150deb3493330a2
Mathis Z <zeroual.mathis@gmail.com>
2026-01-15 11:13:43 +0100

    feat(ui): menu hamburger nav

--- projet-weeb/src/components/Nav/Header.jsx
+++ projet-weeb/src/components/Nav/Header.jsx
@@ -1,19 +1,40 @@
 import { Link } from "react-router-dom";
+import { RxHamburgerMenu, RxCross1 } from "react-icons/rx";
+import { useState } from "react";
 import './Header.css'
 import Button from "../Button/Button";
 
 const Header = () => {
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    // 3. Fonction pour basculer l'état
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return(
         <nav>
             <div>
                 <span className="header-logo"><Link to="/">weeb</Link></span>
-                <Link to="/">À propos</Link>
-                <Link to="/contact">Contact</Link>
+                <Link to="/" className="header-link">À propos</Link>
+                <Link to="/contact" className="header-link">Contact</Link>
             </div>
 
             <div>
-                <Link to="/login">Se connecter</Link>
-                <Button variant="primary">Nous rejoindre</Button>
+                <Link to="/login" className="header-link">Se connecter</Link>
+                <Button variant="primary" className="header-link">Nous rejoindre</Button>
+            </div>
+
+            <div className="hamburger" onClick={toggleMenu}>
+                {isMenuOpen ? <RxCross1 /> : <RxHamburgerMenu />}
+            </div>
+
+            <div className={`header-menu ${isMenuOpen ? "header-open" : ""}`}>
+                <Link to="/" onClick={toggleMenu}>À propos</Link>
+                <Link to="/contact" onClick={toggleMenu}>Contact</Link>
+                <Link to="/login" onClick={toggleMenu}>Se connecter</Link>
+                <Button variant="primary" onClick={toggleMenu}>Nous rejoindre</Button>
             </div>
 
         </nav>

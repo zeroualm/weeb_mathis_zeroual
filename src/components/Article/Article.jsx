836f4de23cc110f537e1282dfcb0050dc2067158
Mathis Z <zeroual.mathis@gmail.com>
2026-06-05 17:43:12 +0200

    fix: remove id

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -5,7 +5,6 @@ const Article = ({ id, title, content, date }) => {
     return (
         <div className="article-card">
             <h2 className="article-title">{title}</h2>
-            <p className="article-content">{id}</p>
             <p className="article-content">{content}</p>
             <span className="article-date">Publié le {new Date(date).toLocaleString('fr-FR')}</span>
         </div>
